99b21dab5e7c45e6ac8f3a2a679011918c5ea11b
ame <25921047+zd613@users.noreply.github.com>
2022-09-19 01:39:58 +0900

    fix: 設定のbaseを修正

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  
+
   // gh pagesでは、 https://アカウント名.github.io/repo名/になるので、baseにrepo名を設定する
-  base: 'syukatsu-sintyoku-app'
+  base: '/syukatsu-sintyoku-app/'
 })
