4b78f4ebb3a510997b8f8f514c49294fa63faf2e
ame <25921047+zd613@users.noreply.github.com>
2022-09-19 20:09:09 +0900

    feat: 会社追加ページを追加

---
+++ src/components/TextInput.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    modelValue: string;
+    name?: string;
+    id?: string;
+    placeholder?: string;
+  }>(),
+  {
+    placeholder: "",
+    name: undefined,
+    id: undefined,
+  }
+);
+
+const emits = defineEmits<{
+  (e: "update:modelValue", newValue: string): void;
+}>();
+
+const internalValue = computed({
+  get: () => {
+    return props.modelValue;
+  },
+  set: (value) => {
+    emits("update:modelValue", value);
+  },
+});
+</script>
+
+<template>
+  <input
+    type="text"
+    :name="name"
+    :id="id"
+    :placeholder="placeholder"
+    v-model="internalValue"
+    class="border border-gray-200 bg-gray-800 p-2 rounded-xl text-gray-200"
+  />
+</template>
---
+++ src/views/AddNewCompany.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import TextInput from "../components/TextInput.vue";
+
+const companyName = ref("");
+const selectedTemplate = ref("0");
+
+const dummyTemplates = [
+  {
+    name: "テンプレート1",
+    progress: ["書類選考", "面談1", "面談(最終)"],
+  },
+  {
+    name: "テンプレート2",
+    progress: ["書類選考", "面談1", "面談2", "面談(最終)"],
+  },
+  {
+    name: "テンプレート3",
+    progress: ["書類選考", "面談1", "課題", "面談2"],
+  },
+];
+
+const router = useRouter();
+const onSubmit = () => {
+  router.push("/");
+};
+</script>
+
+<template>
+  <main class="h-full w-full absolute flex justify-center items-center">
+    <div>
+      <form @submit.prevent="onSubmit">
+        <div class="flex flex-col gap-y-4">
+          <fieldset>
+            <label for="companyName" class="block"
+              >会社名を入力してください。</label
+            >
+
+            <TextInput
+              v-model="companyName"
+              placeholder="会社名"
+              class="mt-2"
+            />
+          </fieldset>
+
+          <fieldset class="mt-8">
+            <label class="block">選考方法を選択してください。</label>
+
+            <div v-for="(template, i) in dummyTemplates" :key="template.name">
+              <input
+                type="radio"
+                :value="i"
+                :name="'template' + i"
+                :id="'template' + i"
+                v-model="selectedTemplate"
+              />
+              <label :for="'template' + i">{{ template.name }}</label>
+            </div>
+          </fieldset>
+
+          <div class="mt-8 flex justify-end">
+            <button type="submit" class="border rounded-xl p-2 mr-4">
+              追加
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </main>
+</template>
