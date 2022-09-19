47860d3114e01c8d28c39c70517c3b1937210601
ame <25921047+zd613@users.noreply.github.com>
2022-09-19 20:36:43 +0900

    feat: 新しい会社追加ページへ遷移するためのボタン追加

---
+++ src/components/RounedButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button type="button" class="bg-purple-800 rounded-full text-white w-16 h-16">
+    <slot />
+  </button>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RounedButton from "../components/RounedButton.vue";
+import IconPlus from "../components/icons/IconPlus.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const onAddNewCompanyButtonClick = () => {
+  router.push("/add-new-company");
+};
+</script>
 
 <template>
-  <main></main>
+  <div>
+    <main></main>
+
+    <div class="fixed w-400 bottom-0 right-0 mr-4 mb-4">
+      <RounedButton
+        class="rounded-button w-12 h-12 shadow-lg"
+        @click="onAddNewCompanyButtonClick"
+      >
+        <IconPlus />
+      </RounedButton>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.rounded-button:active {
+  transform: translateY(2px);
+}
+</style>
