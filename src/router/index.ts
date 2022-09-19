80aad0360001bb0e45cb2ab4e3b7d9c2261e38b4
ame <25921047+zd613@users.noreply.github.com>
2022-09-19 20:11:56 +0900

    chore: 不要なファイルの削除

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,16 +10,9 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
       path: "/add-new-company",
       name: "add-new-company",
+      // lazy-loaded
       component: () => import("../views/AddNewCompany.vue"),
     },
   ],
