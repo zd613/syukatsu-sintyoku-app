562bd11c1f171ee49d2b049b1cd158226aef45a9
ame <25921047+zd613@users.noreply.github.com>
2022-09-19 21:58:43 +0900

    feat: ホームで会社ごとの選考状況を表示するように変更

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,56 @@
 import RounedButton from "../components/RounedButton.vue";
 import IconPlus from "../components/icons/IconPlus.vue";
 import { useRouter } from "vue-router";
+import { useCompanyProgressStore } from "@/stores/project";
+import type { ProgressStep } from "@/types/types";
+
+const companyProgressStore = useCompanyProgressStore();
 
 const router = useRouter();
 const onAddNewCompanyButtonClick = () => {
   router.push("/add-new-company");
 };
+
+// 現在のstepのindexを取得
+function getCurrentStepIndex(progressSteps: ProgressStep[]) {
+  for (let i = 0; i < progressSteps.length; i++) {
+    if (progressSteps[i].status === "waiting") {
+      return i;
+    }
+  }
+  return progressSteps.length;
+}
 </script>
 
 <template>
   <div>
-    <main></main>
+    <main>
+      <div
+        v-for="(progress, i) in companyProgressStore.progressList"
+        :key="i"
+        class="p-2"
+      >
+        <div class="">
+          <span class="border-b-2 text-lg font-bold">{{
+            progress.companyName
+          }}</span>
+        </div>
+        <div>
+          <span
+            v-for="(step, j) in progress.progressSteps"
+            :key="j"
+            :class="
+              j === getCurrentStepIndex(progress.progressSteps)
+                ? 'text-gray-50'
+                : 'text-gray-100/50'
+            "
+          >
+            {{ step.name }} >
+          </span>
+          <span class="text-gray-100/50">内定</span>
+        </div>
+      </div>
+    </main>
 
     <div class="fixed w-400 bottom-0 right-0 mr-4 mb-4">
       <RounedButton
